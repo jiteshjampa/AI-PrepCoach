1f1f4a552d1413330a0cadcbac0de6a93a574075
J JITESH REDDY <79923685+jiteshjampa@users.noreply.github.com>
2024-08-30 18:05:19 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/dashboard");
+  }, []);
   return (
     <div>
       <div>hello jitesh</div>
